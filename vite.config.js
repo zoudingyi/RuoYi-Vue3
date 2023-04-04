256340479d98904350643dd3b40561596b52e4e8
zoudingyi <zoudingyi@qq.com>
2023-04-04 17:04:44 +0800

    :books: chore: edit proxy.

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig(({ mode, command }) => {
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
         '/dev-api': {
-          target: 'https://kat.zx-tech.net/',
+          target: 'https://test-super.zx-tech.net/',
           changeOrigin: true,
           rewrite: p => p.replace(/^\/dev-api/, '')
         }
