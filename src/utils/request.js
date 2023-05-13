06d32d0a788f1b74b3e845b9e41f167529fb1a29
zoudingyi <zoudingyi@qq.com>
2023-05-13 15:49:38 +0800

    :sparkles: feat: :sparkles: feat: 优化响应拦截请求、添加api白名单功能。

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,6 +8,8 @@ import useUserStore from '@/store/modules/user';
 
 // 是否显示重新登录
 export let isRelogin = { show: false };
+// 接口白名单 （响应拦截会过滤掉白名单里面的接口）
+const whiteList = [];
 
 axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8';
 // 创建axios实例
@@ -21,9 +23,9 @@ const service = axios.create({
 // request拦截器
 service.interceptors.request.use(
   config => {
-    // 是否需要设置 token
+    // 是否需要设置token ps:默认需要 不需要则设置headers: { isToken: false }
     const isToken = (config.headers || {}).isToken === false;
-    // 是否需要防止数据重复提交
+    // 是否需要防止数据重复提交 ps:默认需要 不需要则设置headers: { repeatSubmit: false }
     const isRepeatSubmit = (config.headers || {}).repeatSubmit === false;
     if (getToken() && !isToken) {
       config.headers['Authorization'] = 'Bearer ' + getToken(); // 让每个请求携带自定义token 请根据实际情况自行修改
@@ -83,10 +85,10 @@ service.interceptors.request.use(
 // 响应拦截器
 service.interceptors.response.use(
   res => {
-    // 未设置状态码则默认成功状态
-    const code = res.data.code || 200;
+    // 未设置状态码则默认成功状态 （我司no=0为成功状态，当做200处理）
+    const code = res.data.no || 200;
     // 获取错误信息
-    const msg = errorCode[code] || res.data.msg || errorCode['default'];
+    const msg = errorCode[code] || res.data.data || errorCode['default'];
     // 二进制数据则直接返回
     if (
       res.request.responseType === 'blob' ||
@@ -94,7 +96,12 @@ service.interceptors.response.use(
     ) {
       return res.data;
     }
-    if (code === 401) {
+    // 白名单接口直接返回
+    if (whiteList.includes(res.config.url)) {
+      return Promise.resolve(res.data);
+    }
+    // 不同状态处理
+    if (code === -1) {
       if (!isRelogin.show) {
         isRelogin.show = true;
         ElMessageBox.confirm(
@@ -111,7 +118,7 @@ service.interceptors.response.use(
             useUserStore()
               .logOut()
               .then(() => {
-                location.href = '/index';
+                location.href = '/';
               });
           })
           .catch(() => {
@@ -144,6 +151,7 @@ service.interceptors.response.use(
     } else if (message.includes('Request failed with status code')) {
       message = '系统接口' + message.substr(message.length - 3) + '异常';
     }
+
     ElMessage({
       message: message,
       type: 'error',
