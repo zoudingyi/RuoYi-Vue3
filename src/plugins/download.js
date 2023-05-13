827f5a86192e7ee0747b0f1b7fb714a70deaefa4
zoudingyi <zoudingyi@qq.com>
2023-05-13 11:04:23 +0800

    :hammer: refactor: 重新整理download下载方法。

--- src/plugins/download.js
+++ src/plugins/download.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
-import { ElMessage } from 'element-plus';
+import service from '@/utils/request';
+import { ElMessage, ElLoading } from 'element-plus';
 import { saveAs } from 'file-saver';
 import { getToken } from '@/utils/auth';
 import errorCode from '@/utils/errorCode';
-import { blobValidate } from '@/utils/ruoyi';
+import { tansParams, blobValidate } from '@/utils/ruoyi';
 
 const baseURL = import.meta.env.VITE_APP_BASE_API;
+let downloadLoadingInstance;
 
 export default {
   name(name, isDelete = true) {
@@ -75,5 +77,42 @@ export default {
     const rspObj = JSON.parse(resText);
     const errMsg = errorCode[rspObj.code] || rspObj.msg || errorCode['default'];
     ElMessage.error(errMsg);
+  },
+  // 通用下载方法
+  commonDownload(url, params, filename, config) {
+    downloadLoadingInstance = ElLoading.service({
+      text: '正在下载数据，请稍候',
+      background: 'rgba(0, 0, 0, 0.7)'
+    });
+    return service
+      .post(url, params, {
+        transformRequest: [
+          params => {
+            return tansParams(params);
+          }
+        ],
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        responseType: 'blob',
+        ...config
+      })
+      .then(async data => {
+        const isBlob = blobValidate(data);
+        if (isBlob) {
+          const blob = new Blob([data]);
+          saveAs(blob, filename);
+        } else {
+          const resText = await data.text();
+          const rspObj = JSON.parse(resText);
+          const errMsg =
+            errorCode[rspObj.code] || rspObj.msg || errorCode['default'];
+          ElMessage.error(errMsg);
+        }
+        downloadLoadingInstance.close();
+      })
+      .catch(r => {
+        console.error(r);
+        ElMessage.error('下载文件出现错误，请联系管理员！');
+        downloadLoadingInstance.close();
+      });
   }
 };
