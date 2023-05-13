f7b6128a9f169beb2250a88f7a2a26f6ec2ad824
zoudingyi <zoudingyi@qq.com>
2023-05-13 17:17:54 +0800

    :hammer: refactor: 细节调整useTable

--- src/hooks/useTable.js
+++ src/hooks/useTable.js
@@ -1,3 +1,5 @@
+import { reactive, toRefs, onMounted } from 'vue';
+
 /**
  * @description table 页面操作方法封装
  * @param {Function} api 获取表格数据 api 方法(必传)
@@ -7,11 +9,13 @@ export const useTable = (api, options) => {
   const {
     tableRef,
     searchParams = {},
+    pages, // 自定义pages参数
     autorun = true, // 自动执行
     onPreprocess, // 自动执行的预处理
     onSuccess,
     onFinally
   } = options;
+
   const state = reactive({
     loading: false,
     tableData: [],
@@ -22,6 +26,8 @@ export const useTable = (api, options) => {
     total: 0
   });
 
+  if (pages) state.pages = pages;
+
   const getTableData = () => {
     const params = {
       ...searchParams,
@@ -30,11 +36,12 @@ export const useTable = (api, options) => {
     state.loading = true;
     api(params)
       .then(res => {
+        // 返回数据格式层级可能不同，可以自行调整
         const { data, list, total } = res.data;
-        state.tableData = (data || list) ?? [];
-        state.total = total;
+        state.tableData = (data || list || res.data) ?? [];
+        state.total = total ?? res.total;
+        onSuccess && onSuccess(res);
         tableRef && tableRef.value.setScrollTop(0);
-        onSuccess && onSuccess();
       })
       .finally(() => {
         state.loading = false;
@@ -43,7 +50,9 @@ export const useTable = (api, options) => {
   };
 
   onMounted(() => {
+    // 是否自动获取接口数据
     if (autorun) {
+      // 获取接口数据之前的处理
       if (onPreprocess) {
         state.loading = true;
         onPreprocess().then(() => getTableData());
